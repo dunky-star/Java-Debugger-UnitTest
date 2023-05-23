2a314de2dc27cb77548ea01235f7c5b3cc616cd2
dunky-star <dunkygeoffrey39@gmail.com>
2023-05-23 16:32:08 +0300

    First commit

---
+++ DebuggerDemo/src/dunky/debugger/com/Main.java
@@ -0,0 +1,14 @@
+package dunky.debugger.com;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("\nDebugging and Unit Testing in Java");
+        System.out.println("===================================");
+        for(int i = 0; i <= 5; i++){
+
+           System.out.println("i = " +i);
+        }
+    }
+}
+
