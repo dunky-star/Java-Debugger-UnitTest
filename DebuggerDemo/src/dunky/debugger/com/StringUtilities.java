a0efb8c05b797c07f8e30d31a91aa9f5f7db51ac
dunky-star <dunkygeoffrey39@gmail.com>
2023-05-25 09:08:08 +0300

    Field watch points

--- DebuggerDemo/src/dunky/debugger/com/StringUtilities.java
+++ DebuggerDemo/src/dunky/debugger/com/StringUtilities.java
@@ -1,7 +1,6 @@
 package dunky.debugger.com;
 
 public class StringUtilities {
-    StringBuilder sBuilder = new StringBuilder();
     private int charAdded = 0;
 
     public void addChar(StringBuilder sBuilder,  char c){
