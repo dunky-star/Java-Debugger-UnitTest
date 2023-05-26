9795aecdab18aee0d6cbe336f74d61158738e9c1
dunky-star <dunkygeoffrey39@gmail.com>
2023-05-26 12:08:51 +0300

    Unit testing in Java

--- DebuggerDemo/src/dunky/debugger/com/BankAccount.java
+++ DebuggerDemo/src/dunky/debugger/com/BankAccount.java
@@ -14,8 +14,22 @@ public class BankAccount {
     // The branch argument is true if the customer is performing transaction
     // at a branch, with the teller.
     // It's false if the customer is performing the transaction at the ATM.
-    private double deposit(double amount, boolean branch){
+    public double deposit(double amount, boolean branch){
         balance += amount;
         return balance;
     }
+
+    public double withdraw(double amount, boolean branch){
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    // More methods that use firstName, lastName, and perform other functions.
+
+
+
 }
---
+++ DebuggerDemo/src/dunky/debugger/com/BankAccountTest.java
@@ -0,0 +1,22 @@
+package dunky.debugger.com;
+
+/**
+ * A unit is typically a method, therefore, Unit test will typically test a method.
+ */
+
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+    }
+}
