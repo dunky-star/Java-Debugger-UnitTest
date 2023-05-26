fc0d1e5180859d75c16f5d40ed398ecfbb9d19f7
dunky-star <dunkygeoffrey39@gmail.com>
2023-05-26 15:28:39 +0300

    Unit testing in Java

--- DebuggerDemo/src/dunky/debugger/com/BankAccountTest.java
+++ DebuggerDemo/src/dunky/debugger/com/BankAccountTest.java
@@ -10,6 +10,10 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
+        BankAccount account = new BankAccount("Duncan", "Geoffrey Opiyo", 1000.0);
+        double balance = account.deposit(300.0, true);
+        assertEquals(1300.0, balance, 0);
+        assertEquals(1300, account.getBalance(), 0);
     }
 
     @org.junit.Test
@@ -18,5 +22,10 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance() {
+    }
+
+    @org.junit.Test
+    public void dummyTest() {
+        assertEquals(20, 20);
     }
 }
