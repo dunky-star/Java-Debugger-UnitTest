408703df4c2ea289f8e523e5e02f4583f085c16d
dunky-star <dunkygeoffrey39@gmail.com>
2023-05-31 10:02:34 +0300

    Unit testing in Java

--- DebuggerDemo/src/dunky/debugger/com/BankAccountTest.java
+++ DebuggerDemo/src/dunky/debugger/com/BankAccountTest.java
@@ -7,32 +7,39 @@ package dunky.debugger.com;
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
+     private BankAccount account;
+    @org.junit.Before
+    public void setup(){
+        account = new BankAccount("Duncan", "Geoffrey Opiyo", 1000.0, BankAccount.SAVINGS);
+        System.out.println("Running a test...");
+    }
 
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Duncan", "Geoffrey Opiyo", 1000.0, BankAccount.SAVINGS);
         double balance = account.deposit(300.0, true);
         assertEquals(1300.0, balance, 0);
     }
 
     @org.junit.Test
     public void withdraw() {
-        BankAccount account = new BankAccount("Duncan", "Geoffrey Opiyo", 1000.0, BankAccount.SAVINGS);
         double balance = account.withdraw(800.0, true);
         assertEquals(200, balance, 0);
     }
 
     @org.junit.Test
     public void getBalance() {
-        BankAccount account = new BankAccount("Duncan", "Geoffrey Opiyo", 1000.0, BankAccount.SAVINGS);
         account.deposit(300.0, true);
         assertEquals(1300, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isSaving_True(){
-        BankAccount account = new BankAccount("Duncan", "Geoffrey Opiyo", 1000.0, BankAccount.SAVINGS);
         assertTrue(account.isSaving());
+    }
+
+    @org.junit.AfterClass
+    public static void afterClass(){
+        System.out.println("This executes after any test class");
     }
 
 }
